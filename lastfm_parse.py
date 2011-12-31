7c9abd0347a4996168b3a8484123418c016a8106
Jonathan Poulter <poulter7@gmail.com>
2011-12-31 04:09:17 +0000

    HSV colors

--- lastfm_parse.py
+++ lastfm_parse.py
@@ -1,9 +1,16 @@
-import pygame, urllib2, json, datetime, pprint
+import pygame, urllib2, json, pprint
+from random import randint, random
+from datetime import datetime
+from urllib2 import urlopen
+from colorsys import hsv_to_rgb
 
 # setup some useful stuff for display
 pygame.init()
+time_height = 1000
+time_width = 1000
 size=[1000, 1000]
 screen=pygame.display.set_mode(size)
+font = pygame.font.Font(None, 25)
 
 # Define the colors we will use in RGB format
 black = [  0,  0,  0]
@@ -11,23 +18,31 @@ white = [255,255,255]
 blue =  [  0,  0,255]
 green = [  0,255,  0]
 red =   [255,  0,  0]
-font = pygame.font.Font(None, 25)
+
+def seconds_past_midnight(time):
+    return time.hour*3600 + time.minute*60 + time.second
 
 class Track(object):
-    def __init__(self, color = red, name = '', date = 0):
-        self.color = color
-        self.date = date
-        self.size = 3
+    def __init__(self, name = '', datetime = datetime.now()):
+        rgb = hsv_to_rgb(random(), 1, 1)
+        self.color = [i*255 for i in rgb]
+        self.datetime = datetime
+        self.size = 1
 
     def update(self, screen):
-        p = (0,0)
+        time = self.datetime.time()
+        date = self.datetime.date()
+        x = 100
+        y = int(seconds_past_midnight(time)/time_height)
+        p = (x, y)
+        print p
+        print self.color
         pygame.draw.circle(screen, self.color, p, self.size)
-
-
 
 ###
 # Get a list of some tracks
 ###
+
 # open the api_detail file
 f = open('api_details.txt')
 api_key = f.readline()
@@ -38,7 +53,7 @@ f.close()
 user = 'poulter7'
 from_uts = str(0)
 to_uts = str(0)
-response = urllib2.urlopen('http://ws.audioscrobbler.com/2.0/?format=json&from=' +from_uts + 'to='+to_uts+'&method=user.getrecenttracks&user=' + user +'&api_key='+api_key )
+response = urlopen('http://ws.audioscrobbler.com/2.0/?format=json&from=' +from_uts + 'to='+to_uts+'&method=user.getrecenttracks&user=' + user +'&api_key='+api_key )
 json_response = response.read()
 
 # process the response
@@ -48,8 +63,8 @@ track_data = data['recenttracks']['track']
 ###
 # Now some visuals
 ###
-ts = datetime.datetime.fromtimestamp
-tracks = [Track(name = t['name'], date = ts(float(t['date']['uts']))) for t in track_data]
+ts = datetime.fromtimestamp
+tracks = [Track(name = t['name'], datetime = ts(float(t['date']['uts']))) for t in track_data]
 
 def display_data():
     screen.fill(black)
@@ -57,4 +72,5 @@ def display_data():
         t.update(screen)
     pygame.display.update()
 
+display_data()
 pygame.quit()
